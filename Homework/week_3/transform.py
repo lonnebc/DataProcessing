76b4214548d2f63c991ac3b8a877169edf3ad909
lonnebc <lonneke.lammers@hotmail.com>
2015-11-13 22:26:29 +0100

    Update

--- Homework/week_3/transform.py
+++ Homework/week_3/transform.py
@@ -1,49 +1,13 @@
-
 import csv
 
+with open("KNMI_20141231.txt","rb") as lines:
 
-def convert_date(date):
-    """ Takes a date 'YYYYMMDD' and converts it to 'YYYY/MM/DD' """
-    date = [date[0:4], date[4:6], date[6:8]]  # Split in year, month, day respectively
-    return "/".join(date)
+    lines = lines.readlines()[12:]
+with open("knmidata.csv","wb") as f:
+    writer= csv.writer(f)
 
-
-def read_data(year, plot = False):
-    """ Write the knmi temperature-data to a file. """
-
-    with open("KNMI_20151101.txt", "r") as f:
-        lines = f.readlines()
-        data = []
-
-        for line in lines:
-
-            # Text, we can skip this.
-            if line[0] == '#':
-                continue
-
-            line = line.split(',')
-            if line[1][0:4] == str(year):  # take the input year
-                data.append((convert_date(line[1]), line[2][0:-1]))
-
-    if plot:  # Default is False, only to check the html/javascript with.
-        graph_year(data, year)
-
-    with open('knmidata.csv', 'wb') as f:
-            writer = csv.writer(f)
-            for date, temp in data:
-                writer.writerow([date, temp])
-
-
-def graph_year(data, year):
-    """ Plot function to check if my html/javascript was correct. """
-    temp_list = []
-    for date, temp in data:
-        temp_list.append(int(temp))
-    plt.plot(range(len(temp_list)), temp_list)
-    plt.title("Maximum temperature in De Bilt (NL) in {0}".format(year))
-    plt.xlabel("Day in year")
-    plt.ylabel("Temperature in 0.1 degrees Celcius")
-    plt.show()
-
-if __name__ == '__main__':
-    read_data(2014, False)
+    for line in lines:
+        splitted = line.split(',')
+        date = splitted[1][:4] + '/' + splitted[1][4:6] + '/' + splitted[1][6:8] 
+        temperature = splitted[2].strip()
+        writer.writerow([date, temperature])
